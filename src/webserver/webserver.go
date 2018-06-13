c6d0d9888bc7d91e0ab8684c3186c7aa9b7096b1
Nick <nicholas.p.stroupe@gmail.com>
2018-06-12 21:54:41 -0400

    added bootstrap example page

--- src/webserver/webserver.go
+++ src/webserver/webserver.go
@@ -10,9 +10,13 @@ import (
 func homePage(w http.ResponseWriter, r *http.Request) {
    http.ServeFile(w, r, "./static/index.html")
 }
+func BootstrapPage(w http.ResponseWriter, r *http.Request) {
+   http.ServeFile(w, r, "./static/bootstrap.html")
+}
 
 func main() {
    http.HandleFunc("/", homePage) // set router
+   http.HandleFunc("/bootstrap", BootstrapPage) // set router
    err := http.ListenAndServe(":9090", nil) // set listen port
    if err != nil {
       log.Fatal("ListenAndServe: ", err)
